18756a74b1e9a5581d18085d789b72ef183b0699
hpetersenjr <59819459+hpetersenjr@users.noreply.github.com>
2020-04-01 13:48:07 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+import os
 
 app = Flask(__name__)
 
@@ -8,5 +9,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    print('flask is running')
+    app.run(debug="true",host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
