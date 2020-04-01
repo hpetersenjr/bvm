e15658b72ae996230588acd411ae06a4ee75f49d
hpetersenjr <59819459+hpetersenjr@users.noreply.github.com>
2020-04-01 13:32:20 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,4 +8,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run( debug="true" ) #host='0.0.0.0' , port=5010 ,
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    print('flask is running')
