f0e880660be0cd902578dfb62767d5e90a1a5215
hpetersenjr <59819459+hpetersenjr@users.noreply.github.com>
2020-05-19 15:20:46 +0200

    Delete funcInsertData.py

--- app/pyScripts/funcInsertData.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-def insertData( hostIPAddress , dbName , collectionName , documents ):
-            
-    client = pymongo.MongoClient("mongodb://" + hostIPAddress + "/")
-    target = client[dbName][collectionName]
-
-    target.insert_many( documents )
